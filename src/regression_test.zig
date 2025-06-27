c68c1f6966c036b2bf9f70d309acb5bc75f3d7ea
saji <saji@saji.dev>
2025-06-27 09:07:36 -0500

    add equivalence model checking
    now we're cooking. running all tests has slowed down, but worth

--- src/regression_test.zig
+++ src/regression_test.zig
@@ -13,6 +13,45 @@ const TechMap = @import("./gal_techmap.zig").TechMap;
 const jed = @import("./jed.zig");
 const FuseMap = @import("./jed.zig").FuseMap;
 
+fn equivalence(alloc: Allocator, name: []const u8, fmap: FuseMap) !void {
+    const filename = try std.fmt.allocPrint(alloc, "{s}.jed", .{name});
+    defer alloc.free(filename);
+    var out = try std.fs.cwd().createFile(filename, .{});
+    defer out.close();
+    try fmap.writeJed(out, .{});
+
+    var workdir = try std.fs.cwd().openDir("models/", .{});
+    defer workdir.close();
+    const pcf_name = try std.fmt.allocPrint(alloc, "../testcases/{s}.pcf", .{name});
+    defer alloc.free(pcf_name);
+    const vlog_name = try std.fmt.allocPrint(alloc, "../testcases/{s}.v", .{name});
+    defer alloc.free(vlog_name);
+    const jed_backname = try std.fmt.allocPrint(alloc, "../{s}.jed", .{name});
+    defer alloc.free(jed_backname);
+    const args = [_][]const u8{
+        "yosys",
+        "-c",
+        "prove_equiv.tcl",
+        "--",
+        jed_backname,
+        pcf_name,
+        vlog_name,
+    };
+    var proc = std.process.Child.init(&args, alloc);
+    proc.cwd_dir = workdir;
+    proc.stdout_behavior = .Ignore;
+    proc.stderr_behavior = .Ignore;
+    
+    try proc.spawn();
+
+    const res = proc.wait() catch |err| switch (err) {
+        error.FileNotFound => return error.SkipZigTest,
+        else => |other| return other,
+    };
+    try testing.expectEqual(std.process.Child.Term{ .Exited = 0 }, res);
+    try std.fs.cwd().deleteFile(filename);
+}
+
 const Test = struct {
     /// The name of the test file - used to find both the PCF and the netlist json
     name: []const u8,
@@ -51,6 +90,7 @@ fn testFitterImpl(alloc: Allocator, t: Test) anyerror!void {
     defer fmap.deinit();
     try gal.synthesize(&fmap);
     try jed.testJedutil(alloc, fmap, .jed);
+    try equivalence(alloc, t.name, fmap);
 }
 
 fn testFitter(t: Test) !void {
@@ -58,17 +98,20 @@ fn testFitter(t: Test) !void {
     // try testing.checkAllAllocationFailures(alloc, testFitterImpl, .{t});
     try testFitterImpl(alloc, t);
 }
-test "regression_olmc_test" {
+test "regression olmc_test" {
     try testFitter(.{ .name = "olmc_test" });
 }
-test "regression_big_xor" {
+test "regression big_xor" {
     try testFitter(.{ .name = "big_xor" });
 }
-test "regression_tiny_xor" {
+test "regression tiny_xor" {
     try testFitter(.{ .name = "tiny_xor" });
 }
-test "regression_tristate" {
+test "regression tristate" {
     try testFitter(.{ .name = "tristate" });
+}
+test "regression complex_single_sop" {
+    try testFitter(.{ .name = "complex_single_sop" });
 }
 // test "regression_and_gate" {
 //     try testFitter(.{
