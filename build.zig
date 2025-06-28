a51e92315ed6c9d1c9fd8f7237d91f41736c8ba0
saji <saji@saji.dev>
2025-06-28 14:41:28 -0500

    add test filter thing
    running the regressions can take a while so we want to be able to target
    one while iterating

--- build.zig
+++ build.zig
@@ -101,12 +101,14 @@ pub fn build(b: *std.Build) void {
     // but does not run it.
     const lib_unit_tests = b.addTest(.{
         .root_module = lib_mod,
+        .filters = b.args orelse &.{},
     });
 
     const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
 
     const exe_unit_tests = b.addTest(.{
         .root_module = exe_mod,
+        .filters = b.args orelse &.{},
     });
 
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
